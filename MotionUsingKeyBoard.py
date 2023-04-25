8af1742552b22e456c033f57e41d8b80de6d6559
NIDHI SHARMA <114356358+Nidhi-Sharma9419@users.noreply.github.com>
2023-04-25 22:58:46 +0530

    Create MotionUsingKeyBoard.py

---
+++ MotionUsingKeyBoard.py
@@ -0,0 +1,55 @@
+import pygame
+
+pygame.init()
+
+screen_width = 1280
+screen_height = 720
+
+BLACK = (0,0,0)
+RED = (225, 0, 0)
+#create screen
+screen = pygame.display.set_mode([1280,720])
+
+object_color = (255, 0, 0)
+
+# Set the dimensions of the object
+object_width = 50
+object_height = 50
+
+# Set the initial position of the object
+object_x = 0   #Don't know how this is working?
+object_y = 0
+
+pygame.draw.rect(screen, object_color, (object_x, object_y, object_width, object_height))
+
+test = False
+while not test:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            test = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                object_x = object_x-50
+            if event.key == pygame.K_RIGHT:
+                object_x = object_x+10
+            if event.key == pygame.K_UP:
+                object_y = object_y-10
+            if event.key == pygame.K_DOWN:
+                object_y = object_y+10
+        screen.fill(BLACK)
+        #crate rectangle
+        surface = pygame.Surface([100,100])
+        surface.fill(RED)
+        rectangle=surface.get_rect()
+        #give position of our rectangle
+        rectangle.x=600
+        rectangle.y = 400
+        #move rectangle
+        rectangle.x = rectangle.x+ object_x
+        rectangle.y = rectangle.y + object_y
+        #draw rectangle
+        pygame.draw.rect(screen, RED, rectangle, 0)
+
+        pygame.display.flip() 
+
+pygame.quit()
