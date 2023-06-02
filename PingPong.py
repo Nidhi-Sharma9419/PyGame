e33499d29d140915460bc3d5b2548e5d5fbc86c2
Nidhi-Sharma9419 <114356358+Nidhi-Sharma9419@users.noreply.github.com>
2023-06-02 17:45:58 +0530

    Update PingPong.py

--- PingPong.py
+++ PingPong.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, random
 import websocket
 from pygame.event import Event
 import threading
@@ -25,10 +25,12 @@ def point_won(winner):
     global cpu_points, player_points
 
     if winner == "cpu":
-        cpu_points += 1
+        #cpu_points += 1
+        print('1')
     if winner == "player":
         #player_points += 1
-        pygame.quit
+        #pygame.quit
+        print('2')
 
 player1_up = pygame.event.custom_type() #No use ok it is for you to use this
 player1_down = pygame.event.custom_type()
@@ -70,17 +72,33 @@ wsapp_thread.start()
 
 def animate_bally():
     global bally_speed_x, bally_speed_y
+    global cpu_points, player_points
     bally.x+= bally_speed_x   #this will increase each by pixel
     bally.y+= bally_speed_y
 
     if bally.bottom >= screen_height or bally.top<= 0:
        bally_speed_y *= -1
 
-    if bally.right >= screen_width:
+    '''if bally.right >= screen_width: #ye hum bana rahe the score diplay karne ke liye
+        cpu_points+=1
         point_won("cpu")
+        pygame.quit()
+        
         
     if bally.left <= 0:
+        player_points+=1
         point_won("player")
+        pygame.quit()'''
+
+    if bally.left <= 0: 
+        bally_start()
+        player_points += 1
+
+	# Opponent Score
+    if bally.right >= screen_width:
+        bally_start()
+        cpu_points += 1    
+        
 
     if bally.colliderect(player) or bally.colliderect(cpu):
         bally_speed_x *= -1
@@ -102,6 +120,13 @@ def  animate_cpu():
     if cpu.bottom>= screen_height:
         cpu.bottom = screen_height
 
+
+def bally_start():
+    global bally_speed_x, bally_speed_y
+
+    bally.center = (screen_width/2, screen_height/2)
+    bally_speed_y *= random.choice((1,-1))
+    bally_speed_x *= random.choice((1,-1))
 
 pygame.init()
 
@@ -146,8 +171,21 @@ player.midright = (screen_width, screen_height/2)
 bally_speed_x = 6
 bally_speed_y = 6
 
+
 player_speed = 0
 cpu_speed = 0
+
+player_points=0
+cpu_points=0
+
+bally_speed_x = 6 * random.choice((1,-1))
+bally_speed_y = 6 * random.choice((1,-1))
+
+
+
+basic_font = pygame.font.Font('freesansbold.ttf', 32)
+
+
 
 
 '''Now GAME LOOP part''' 
@@ -214,18 +252,25 @@ while True:
     #Draw game objects
     screen.fill(BLACK) #does not leave mark when object moves
     #crate rectangle
-    surface = pygame.Surface([100,100])
-    surface.fill(RED)
-    rectangle=surface.get_rect()
+    #surface = pygame.Surface([100,100])
+    #surface.fill(RED)
+    #rectangle=surface.get_rect()
     #give position of our rectangle
-    rectangle.x=640
-    rectangle.y = 300
+    #rectangle.x=640
+    #rectangle.y = 300
     
     #draw rectangle
     pygame.draw.aaline(screen, 'black', (screen_width/2,0), (screen_width/2, screen_height))
     pygame.draw.ellipse(screen, RED, bally)
     pygame.draw.rect(screen, 'black', cpu)
     pygame.draw.rect(screen, 'black', player)
+
+    player_text = basic_font.render(f'{player_points}',False,'grey')
+    screen.blit(player_text,(660,470))
+
+    cpu_text = basic_font.render(f'{cpu_points}',False,'grey')
+    screen.blit(cpu_text,(600,470))
+
     pygame.display.flip()     
     #Update the diplay
     pygame.display.update
@@ -235,6 +280,8 @@ while True:
     #It takes an integer as an argument an that integer is the number of frames per second that we want
     clock.tick(50)
 
+pygame.display.quit()
+pygame.quit()
 
     #How to draw things on the surface
     
